5228b25b400e7d5e923d7317e2a21b4e29d3623d
Rodrigo <rodrigo.carvalho@ga.gov.au>
2021-08-19 14:35:29 +1000

    check-dead-queues.py

---
+++ check-dead-queues/check-dead-queues.py
@@ -0,0 +1,38 @@
+from textwrap import dedent
+
+import boto3
+
+
+def check_deadletter_queues():
+    client = boto3.client('sqs')
+    queues = client.list_queues()
+
+    if queues.get('QueueUrls') is None:
+        print("No queues found")
+
+    bad_queues = []
+    for queue in queues['QueueUrls']:
+        if 'deadletter' in queue:
+            attributes_dict = client.get_queue_attributes(
+                QueueUrl=queue,
+                AttributeNames=['ApproximateNumberOfMessages']
+            )
+            queue_size = int(attributes_dict['Attributes'].get("ApproximateNumberOfMessages"))
+            if queue_size > 0:
+                bad_queues.append(f"SQS deadletter queue {queue} has {queue_size} items on it.")
+
+    bad_queues_str = "\n".join(f" * {q}" for q in bad_queues)
+    message = dedent(
+        f"""
+            Found {len(bad_queues)} dead queues that have messages on them.
+            These are the culprits:
+            {bad_queues_str}
+        """
+    )
+
+    if len(bad_queues) > 0:
+        raise Exception(message)
+
+
+if __name__ == "__main__":
+    check_deadletter_queues()
