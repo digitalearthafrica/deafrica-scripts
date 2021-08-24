64384cd13a238c4cafacefb4a68093b095b31e3e
Rodrigo <rodrigo.carvalho@ga.gov.au>
2021-08-24 16:58:04 +1000

    check dead queues ready

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,9 @@
 import boto3
 import pytest
 from moto import mock_sqs
+from odc.aws.queue import publish_messages, get_queue, publish_message
+
+from deafrica_automation_tools.check_dead_queues import get_dead_queues, check_deadletter_queues
 
 REGION = "af-south-1"
 
@@ -12,61 +15,55 @@ def setup_env(monkeypatch):
 
 @mock_sqs
 def test_get_dead_queues(monkeypatch):
-    sns_client = boto3.client("sqs", region_name=REGION)
-    sns_client.create_queue(QueueName="deafrica-test-queue-deadletter")
-    sns_client.create_queue(QueueName="deafrica-test-queue2-deadletter")
-    sns_client.create_queue(QueueName="deafrica-test-queue3-deadletter")
-    sns_client.create_queue(QueueName="deafrica-test-queue")
+    resource = boto3.resource("sqs")
+    resource.create_queue(QueueName="deafrica-test-queue-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue2-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue3-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue")
 
-    from deafrica_automation_tools.check_dead_queues import get_dead_queues
-
-    dead_queues = get_dead_queues(region=REGION)
+    dead_queues = get_dead_queues()
 
     assert len(dead_queues) == 3
 
 
 @mock_sqs
 def test_get_find_msg_dead_queues(monkeypatch):
-    sqs_client = boto3.client('sqs', region_name=REGION)
-    queue1 = sqs_client.create_queue(QueueName="deafrica-test-queue-deadletter")
-    queue2 = sqs_client.create_queue(QueueName="deafrica-test-queue2-deadletter")
-    queue3 = sqs_client.create_queue(QueueName="deafrica-test-queue3-deadletter")
-    queue4 = sqs_client.create_queue(QueueName="deafrica-test-queue")
+    resource = boto3.resource("sqs")
+    queue1 = resource.create_queue(QueueName="deafrica-test-queue-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue2-deadletter")
+    queue3 = resource.create_queue(QueueName="deafrica-test-queue3-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue")
 
     message = 'Message Body to dead queue'
-    sqs_client.send_message(
-        QueueUrl=queue1['QueueUrl'],
-        MessageBody=message,
+    publish_message(
+        queue=queue1,
+        message=message,
     )
-    sqs_client.send_message(
-        QueueUrl=queue3['QueueUrl'],
-        MessageBody=message,
+    publish_message(
+        queue=queue3,
+        message=message,
     )
 
-    from deafrica_automation_tools.check_dead_queues import get_dead_queues, check_deadletter_queues
-
-    dead_queues = get_dead_queues(REGION)
+    dead_queues = get_dead_queues()
 
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        check_deadletter_queues(dead_queues=dead_queues, region=REGION)
+        check_deadletter_queues(dead_queues=dead_queues)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
 
 
 @mock_sqs
 def test_get_no_msg_dead_queues(monkeypatch):
-    sqs_client = boto3.client('sqs', region_name=REGION)
-    sqs_client.create_queue(QueueName="deafrica-test-queue-deadletter")
-    sqs_client.create_queue(QueueName="deafrica-test-queue2-deadletter")
-    sqs_client.create_queue(QueueName="deafrica-test-queue3-deadletter")
-    sqs_client.create_queue(QueueName="deafrica-test-queue")
+    resource = boto3.resource("sqs")
+    resource.create_queue(QueueName="deafrica-test-queue-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue2-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue3-deadletter")
+    resource.create_queue(QueueName="deafrica-test-queue")
 
-    from deafrica_automation_tools.check_dead_queues import get_dead_queues, check_deadletter_queues
-
-    dead_queues = get_dead_queues(REGION)
+    dead_queues = get_dead_queues()
 
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        check_deadletter_queues(dead_queues=dead_queues, region=REGION)
+        check_deadletter_queues(dead_queues=dead_queues)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 0
 
