5412730ac85b6b1126732e325f4975853ec8fc41
Rodrigo <rodrigo.carvalho@ga.gov.au>
2021-09-22 12:49:18 +1000

    add dev flag

--- monitoring/tools/s2_gap_report.py
+++ monitoring/tools/s2_gap_report.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 import click
 import pandas as pd
-from odc.aws import s3_client, s3_dump
-from odc.aws.inventory import list_inventory, find_latest_manifest
+from odc.aws import s3_dump
+from odc.aws.inventory import list_inventory
 from urlpath import URL
 
 from monitoring.tools.utils import send_slack_notification
@@ -20,6 +20,7 @@ log.addHandler(console)
 
 AFRICA_TILES = "https://raw.githubusercontent.com/digitalearthafrica/deafrica-extent/master/deafrica-mgrs-tiles.csv.gz"
 SENTINEL_2_STATUS_REPORT_PATH = URL("s3://deafrica-sentinel-2/status-report/")
+SENTINEL_2_DEV_STATUS_REPORT_PATH = URL("s3://deafrica-sentinel-2-dev/status-report/")
 SENTINEL_2_INVENTORY_PATH = URL(
     "s3://deafrica-sentinel-2-inventory/deafrica-sentinel-2/deafrica-sentinel-2-inventory/"
 )
@@ -36,7 +37,6 @@ def get_and_filter_cogs_keys():
     Retrieve key list from a inventory bucket and filter
     :return:
     """
-    # s3 = s3_client(region_name=COGS_REGION, aws_unsigned=True)
     source_keys = list_inventory(
         manifest=f"{SENTINEL_COGS_INVENTORY_PATH}",
         prefix=COGS_FOLDER_NAME,
@@ -62,14 +62,22 @@ def get_and_filter_cogs_keys():
     )
 
 
-def generate_buckets_diff(update_stac: bool = False, slack_url: str = None) -> None:
+def generate_buckets_diff(
+    update_stac: bool = False, dev: bool = False, slack_url: str = None
+) -> None:
     """
     Compare Sentinel-2 buckets in US and Africa and detect differences
     A report containing missing keys will be written to s3://deafrica-sentinel-2/status-report
     :param update_stac: (bool) Define if the report will contain all scenes from the source for an update
+    :param dev: (bool) Define if the task is running in dev environment
     :param slack_url: (str) Optional slack URL in case of you want to send a slack notification
     """
     log.info("Process started")
+
+    # defines where the report will be saved
+    s2_status_report_path = (
+        SENTINEL_2_DEV_STATUS_REPORT_PATH if dev else SENTINEL_2_STATUS_REPORT_PATH
+    )
 
     date_string = datetime.now().strftime("%Y-%m-%d")
 
@@ -108,25 +116,23 @@ def generate_buckets_diff(update_stac: bool = False, slack_url: str = None) -> N
 
         output_filename = URL(f"{date_string}.txt.gz")
 
-    log.info(f"File will be saved in {SENTINEL_2_STATUS_REPORT_PATH}{output_filename}")
+    log.info(f"File will be saved in {s2_status_report_path}{output_filename}")
 
     s3_dump(
         data=gzip.compress(str.encode("\n".join(missing_scenes))),
-        url=str(URL(SENTINEL_2_STATUS_REPORT_PATH) / output_filename),
+        url=str(URL(s2_status_report_path) / output_filename),
         s3=None,
         ContentType="application/gzip",
     )
 
     log.info(f"10 first missing_scenes {list(missing_scenes)[0:10]}")
-    log.info(
-        f"Wrote inventory to: {str(URL(SENTINEL_2_STATUS_REPORT_PATH) / output_filename)}"
-    )
+    log.info(f"Wrote inventory to: {str(URL(s2_status_report_path) / output_filename)}")
 
     if len(orphaned_keys) > 0:
         output_filename = URL(f"{date_string}_orphaned.txt")
         s3_dump(
             data=gzip.compress(str.encode("\n".join(orphaned_keys))),
-            url=str(URL(SENTINEL_2_STATUS_REPORT_PATH) / output_filename),
+            url=str(URL(s2_status_report_path) / output_filename),
             s3=None,
             ContentType="application/gzip",
         )
@@ -155,17 +161,23 @@ def generate_buckets_diff(update_stac: bool = False, slack_url: str = None) -> N
     help="Defines if all stacs need to be updated",
 )
 @click.option(
+    "--dev",
+    is_flag=True,
+    default=False,
+    help="Defines if all stacs need to be updated",
+)
+@click.option(
     "--slack_url",
     help="Slack url to use to send a notification",
     default=None,
 )
 @click.command("s2-gap-report")
-def cli(update_stac: bool = False, slack_url: str = None):
+def cli(update_stac: bool = False, dev: bool = False, slack_url: str = None):
     """
     Publish missing scenes
     """
     try:
-        generate_buckets_diff(update_stac=update_stac, slack_url=slack_url)
+        generate_buckets_diff(update_stac=update_stac, dev=dev, slack_url=slack_url)
     except Exception as error:
         log.exception(error)
         traceback.print_exc()
