9662a63089b3e7706f740444ac82ec17ca5d4cc3
Seffat Chowdhury <schowdhury@sansa.org.za>
2022-06-08 09:37:40 +0200

    Reformat

--- deafrica/tests/test_latency_checker.py
+++ deafrica/tests/test_latency_checker.py
@@ -3,6 +3,7 @@ import pytest
 
 
 def test_latency_checker():
+    pass
     # assert latency_checker("asdasd", 3, None) == -1
     # assert latency_checker("s2_l2a", -1, None) == -1
     # assert latency_checker("s2_l2a", 3, None) == 0
