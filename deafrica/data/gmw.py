164d8b918c1998c836724d45482dadd7e83bcd54
Victoria Neema <victoria@kartoza.com>
2024-06-13 11:25:47 +0000

    Update script to upload invidual tiles

--- deafrica/data/gmw.py
+++ deafrica/data/gmw.py
@@ -1,6 +1,6 @@
 """
 Download the Global Mangrove Watch (1996 - 2020) Version 3.0 Dataset
-for a year from Zenodo, merge and convert to Cloud Optimized Geotiff,
+for a year from Zenodo, convert to CLoud Optimized Geotiff,
 and push to an S3 bucket
 
 Datasource: https://zenodo.org/records/6894273
@@ -12,6 +12,7 @@ import shutil
 from datetime import datetime
 from pathlib import Path
 from subprocess import STDOUT, check_output
+from typing import List, Set
 from zipfile import ZipFile
 
 import click
@@ -22,7 +23,6 @@ from odc.aws import s3_dump
 from pystac import Item
 from rio_stac import create_stac_item
 from urlpath import URL
-from typing import List, Set
 
 from deafrica.utils import odc_uuid, send_slack_notification, setup_logging, slack_url
 
@@ -42,7 +42,7 @@ VALID_YEARS = [
 SOURCE_URL_PATH = URL("https://zenodo.org/records/6894273/files/")
 FILE_NAME = "gmw_v3_{year}_gtiff.zip"
 LOCAL_DIR = Path(os.getcwd())
-AFRICA_EXTENT_URL = "https://raw.githubusercontent.com/digitalearthafrica/deafrica-extent/master/africa-extent-bbox.json" # noqa E501
+AFRICA_EXTENT_URL = "https://raw.githubusercontent.com/digitalearthafrica/deafrica-extent/master/africa-extent-bbox.json"  # noqa E501
 
 # Set log level to info
 log = setup_logging()
@@ -99,72 +99,6 @@ def get_gmw_africa_tiles() -> Set[str]:
     return gmw_africa_tiles
 
 
-def create_and_upload_stac(cog_file: Path, s3_dst: str, year) -> Item:
-    out_path = URL(f"{s3_dst}/{year}/")
-
-    log.info("Item base creation")
-    item = create_stac_item(
-        str(cog_file),
-        id=str(odc_uuid("gmw", "3.0", [cog_file.name.replace("tif", "")])),
-        with_proj=True,
-        input_datetime=datetime(int(year), 12, 31),
-        properties={
-            "odc:product": "gmw",
-            "start_datetime": f"{year}-01-01T00:00:00Z",
-            "end_datetime": f"{year}-12-31T23:59:59Z",
-        },
-    )
-
-    log.info("links creation")
-    item.set_self_href(str(out_path / f"gmw_{year}_stac-item.json"))
-    item.add_links(
-        [
-            pystac.Link(
-                target=str(SOURCE_URL_PATH / FILE_NAME.format(year=year)),
-                title="Source file",
-                rel=pystac.RelType.DERIVED_FROM,
-                media_type="application/zip",
-            )
-        ]
-    )
-
-    out_data = out_path / cog_file.name
-    # Remove asset created by create_stac_item and add our own
-    del item.assets["asset"]
-    item.assets["mangrove"] = pystac.Asset(
-        href=str(out_data),
-        title="gmw-v3.0",
-        media_type=pystac.MediaType.COG,
-        roles=["data"],
-    )
-
-    log.info(f"Item created {item.to_dict()}")
-    
-    # Error _RefResolutionError: Unresolvable JSON pointer: 'definitions/link'
-    # not sure why.
-    # log.info(f"Item validated {item.validate()}")
-
-    log.info(f"Dump the data to S3 {str(cog_file)}")
-    s3_dump(
-        data=open(str(cog_file), "rb").read(),
-        url=str(out_data),
-        ACL="bucket-owner-full-control",
-        ContentType="image/tiff",
-    )
-    log.info(f"File written to {out_data}")
-
-    log.info("Write STAC to S3")
-    s3_dump(
-        data=json.dumps(item.to_dict(), indent=2),
-        url=item.self_href,
-        ACL="bucket-owner-full-control",
-        ContentType="application/json",
-    )
-    log.info(f"STAC written to {item.self_href}")
-
-    return item
-
-
 def gmw_download_stac_cog(year: str, s3_dst: str, slack_url: str = None) -> None:
     """
     Mangrove download, COG and STAC process
@@ -185,35 +119,83 @@ def gmw_download_stac_cog(year: str, s3_dst: str, slack_url: str = None) -> None
             if any(label in file for label in gmw_africa_tiles)
         ]
 
-        tiff_list = LOCAL_DIR / "files_to_merge.txt"
-        with open(tiff_list, "w") as file:
-            for item in gmw_africa_files:
-                file.write(item + "\n")
+        # Copy over the tiles one by one.
+        for local_file in gmw_africa_files:
+            filename = os.path.splitext(os.path.basename(local_file))[0]
+            region_code = filename.split("_")[1]
+            out_cog = URL(s3_dst) / str(year) / region_code / f"{filename}.tif"
+            out_stac = (
+                URL(s3_dst) / str(year) / region_code / f"{filename}.stac-item.json"
+            )
 
-        output_file = LOCAL_DIR / f"gmw_v3_{year}.tiff"
-        log.info(f"Output TIF file is {output_file}")
+            ## Create and upload COG.
 
-        log.info("Start gdal_merge.py")
-        cmd = (
-            f"gdal_merge.py -o {str(output_file)} -of GTiff -ot Byte "
-            "-ps 0.0002 0.0002 -ul_lr -26.36 38.35 64.50 -47.97 -v -a_nodata 0 "
-            "--optfile files_to_merge.txt"
-        )
-        # Disable disk space check
-        cmd = f"CHECK_DISK_FREE_SPACE=FALSE {cmd}"
-        check_output(cmd, stderr=STDOUT, shell=True)
-        log.info(f"File {output_file} merged successfully")
+            ds = rioxarray.open_rasterio(local_file).squeeze(dim="band")
+            # Subset to Africa
+            ulx, uly, lrx, lry = AFRICA_BBOX
+            # Note: lats are upside down!
+            ds = ds.sel(y=slice(uly, lry), x=slice(ulx, lrx))
+            # Add crs information
+            ds = assign_crs(ds, crs="EPSG:4326")
+            # Create an in memory COG.
+            cog_bytes = write_cog(
+                geo_im=ds, fname=":mem:", nodata=0, overview_resampling="nearest"
+            )
+            # Upload COG to s3
+            s3_dump(
+                data=cog_bytes,
+                url=str(out_cog),
+                ACL="bucket-owner-full-control",
+                ContentType="image/tiff",
+            )
+            log.info(f"COG written to {out_cog}")
 
-        log.info("Creating cloud optimized GeoTIFF ...")
-        cloud_optimized_file = LOCAL_DIR / f"deafrica_gmw_{year}.tif"
-        cmd = (
-            "CHECK_DISK_FREE_SPACE=FALSE rio cogeo create --overview-resampling "
-            f"nearest {output_file} {cloud_optimized_file}"
-        )
-        check_output(cmd, stderr=STDOUT, shell=True)
-        log.info(f"File {cloud_optimized_file} cloud optimized successfully")
+            ## Create and upload STAC.
 
-        create_and_upload_stac(cog_file=cloud_optimized_file, s3_dst=s3_dst, year=year)
+            # Base item creation.
+            item = create_stac_item(
+                out_cog,
+                id=str(odc_uuid("gmw", "3.0", [out_cog.name.replace("tif", "")])),
+                with_proj=True,
+                input_datetime=datetime(int(year), 12, 31),
+                properties={
+                    "odc:product": "gmw",
+                    "odc:region_code": region_code,
+                    "start_datetime": f"{year}-01-01T00:00:00Z",
+                    "end_datetime": f"{year}-12-31T23:59:59Z",
+                },
+            )
+            # Links creation
+            item.set_self_href(str(out_stac))
+            item.add_links(
+                [
+                    pystac.Link(
+                        target=str(SOURCE_URL_PATH / FILE_NAME.format(year=year)),
+                        title="Source file",
+                        rel=pystac.RelType.DERIVED_FROM,
+                        media_type="application/zip",
+                    )
+                ]
+            )
+            # Remove asset created by create_stac_item and add our own
+            del item.assets["asset"]
+            item.assets["mangrove"] = pystac.Asset(
+                href=str(out_cog),
+                title="gmw-v3.0",
+                media_type=pystac.MediaType.COG,
+                roles=["data"],
+            )
+            log.info(f"Item created {item.to_dict()}")
+            # Not sure why this takes too long.
+            # log.info(f"Item validated {item.validate()}")
+            # Upload STAC to s3
+            s3_dump(
+                data=json.dumps(item.to_dict(), indent=2),
+                url=item.self_href,
+                ACL="bucket-owner-full-control",
+                ContentType="application/json",
+            )
+            log.info(f"STAC written to {item.self_href}")
 
         # All done!
         log.info(f"Completed work on {s3_dst}/{year}")
