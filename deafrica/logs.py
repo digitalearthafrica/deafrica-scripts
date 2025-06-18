2f53ec271b21343b156658f11b731149cb61e3fe
vikineema <vikinyaga@gmail.com>
2025-06-18 12:00:18 +0300

    update logger setup

--- deafrica/logs.py
+++ deafrica/logs.py
@@ -4,7 +4,10 @@ import logging
 def setup_logging(level: int = logging.INFO) -> logging.Logger:
     """Set up a simple logger"""
     log = logging.getLogger(__name__)
-    console = logging.StreamHandler()
-    log.addHandler(console)
     log.setLevel(level)
+
+    if not log.handlers:
+        console = logging.StreamHandler()
+        log.addHandler(console)
+
     return log
